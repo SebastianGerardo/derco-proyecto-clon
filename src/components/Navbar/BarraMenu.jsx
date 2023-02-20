3ce7f50dcd9d8821e0209c88187cba8a743fe694
Sebastián Gerardo <sebastiangerardo27@gmail.com>
2023-02-20 18:32:48 -0500

    Añadido de Menu desplegable

--- src/components/Navbar/BarraMenu.jsx
+++ src/components/Navbar/BarraMenu.jsx
@@ -1,18 +1,26 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export const BarraMenu = () => {
     return (
-        <div className="w-full">
-            <div className="p-5">
+        <div className="h-full overflow-hidden relative flex flex-col justify-center">
+            <div className="w-full h-16 absolute top-0">
                 <img
                     src="https://app.elipse.ai/hs-fs/hubfs/Derco%20Center%20Logo%20Blanco.png?width=1920&name=Derco%20Center%20Logo%20Blanco.png"
                     alt="Desarrollo Global"
-                    className="mx-auto "
+                    className="mx-auto min-w-full min-h-full object-cover"
                 />
             </div>
-            <div className="mb-5 space-y-2 px-6">
-                <Link className="flex items-center gap-5 text-white font-medium text-lg mt-5" to={"/dashboard/anfitrion"}><i className="fa-solid fa-briefcase"></i>Anfitrion</Link>
+            <div className="space-y-2 px-6 flex flex-col justify-center h-full ">
+                <NavLink className="flex items-center gap-6 text-white font-medium text-lg py-3" to={"/dashboard/anfitrion"} style={({ isActive }) => isActive ? {color: '#3e3e3e'} : undefined}><i className="fa-solid fa-briefcase"></i>Anfitrion</NavLink>
+                <NavLink className="flex items-center gap-6 text-white font-medium text-lg py-3" to={"/dashboard/recepcion"} style={({ isActive }) => isActive ? {color: '#3e3e3e'} : undefined}><i className="fa-solid fa-briefcase"></i>Recepcion</NavLink>
+                <NavLink className="flex items-center gap-6 text-white font-medium text-lg py-3" to={"/dashboard/asignacion"} style={({ isActive }) => isActive ? {color: '#3e3e3e'} : undefined}><i className="fa-solid fa-briefcase"></i>Asignacion</NavLink>
+                <NavLink className="flex items-center gap-6 text-white font-medium text-lg py-3" to={"/dashboard/servicio"} style={({ isActive }) => isActive ? {color: '#3e3e3e'} : undefined}><i className="fa-solid fa-briefcase"></i>Servicio</NavLink>
+                <NavLink className="flex items-center gap-6 text-white font-medium text-lg py-3" to={"/dashboard/lavado"} style={({ isActive }) => isActive ? {color: '#3e3e3e'} : undefined}><i className="fa-solid fa-briefcase"></i>Lavado</NavLink>
+                <NavLink className="flex items-center gap-6 text-white font-medium text-lg py-3" to={"/dashboard/secado"} style={({ isActive }) => isActive ? {color: '#3e3e3e'} : undefined}><i className="fa-solid fa-briefcase"></i>Secado</NavLink>
+                <NavLink className="flex items-center gap-6 text-white font-medium text-lg py-3" to={"/dashboard/entrega"} style={({ isActive }) => isActive ? {color: '#3e3e3e'} : undefined}><i className="fa-solid fa-briefcase"></i>Entrega</NavLink>
             </div>
         </div>
     );
 };
+
+
--- src/components/Navbar/Menu.jsx
+++ src/components/Navbar/Menu.jsx
@@ -2,7 +2,7 @@ import { BarraMenu } from "./BarraMenu";
 
 export const Menu = () => {
   return (
-    <div className="space-y-2 w-[18%] shadow-lg bg-redDerco hidden lg:block xl:block z-100 h-screen">
+    <div className=" space-y-2 shadow-lg bg-redDerco hidden lg:block xl:block z-100 h-screen transition-all w-16 hover:w-64">
       <BarraMenu />
     </div>
   );
