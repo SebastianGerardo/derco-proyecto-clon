c5fb4579b9bc2805e71d78d80358d3bd8a28911e
Maxsoll159 <mar_1905@hotmail.es>
2023-02-22 22:57:16 -0500

    Animaciones, menu

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import { Principal } from "./Principal";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import '../node_modules/@fortawesome/fontawesome-free/css/all.css'
+import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
 import { ContextDerco } from "./context/ContextDerco";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ContextDerco>
-    <BrowserRouter>
-      <Principal />
-    </BrowserRouter>
+      <BrowserRouter>
+        <Principal />
+      </BrowserRouter>
     </ContextDerco>
   </React.StrictMode>
 );
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -7,9 +7,12 @@ export const Login = () => {
         navigate("/dashboard")
     }
     return (
-        <section className="">
+        <section>
+            <div className="bg"></div>
+<div className="bg bg2"></div>
+<div className="bg bg3"></div>
             <div className="container mx-auto flex items-center align-center w-screen h-screen">
-                <div className="w-11/12 mx-auto drop-shadow-2x xl:w-[30%] lg:w-1/4 md:w-1/2 shadow-xl">
+                <div className="w-11/12 mx-auto drop-shadow-2x xl:w-[30%] lg:w-1/4 md:w-1/2 shadow-xl bg-white">
                     <div className=" bg-redDerco mx-auto p-5">
                         <img src="https://app.elipse.ai/hs-fs/hubfs/Derco%20Center%20Logo%20Blanco.png?width=1920&name=Derco%20Center%20Logo%20Blanco.png" alt="dERCO" className="w-36 mx-auto" />
                     </div>
@@ -21,12 +24,12 @@ export const Login = () => {
 
                             <div className=" relative border-2 focus-within:border-blue-500 rounded-md">
                                 <input type="email" name="email" placeholder=" " className="block p-4 w-full text-base appearance-none focus:outline-none bg-transparent" />
-                                <label  className="absolute top-0 text-base bg-white p-4 -z-1 duration-300 origin-0 text-gray-400">Correo</label>
+                                <label  className="z-1 absolute top-0 text-base bg-white p-4 duration-300 origin-0 text-gray-400">Correo</label>
                             </div>
 
                             <div className=" relative border-2 focus-within:border-blue-500 mt-5 rounded-md">
                                 <input type="password" name="email" placeholder=" " className="block p-4 w-full text-base appearance-none focus:outline-none bg-transparent" />
-                                <label  className="absolute top-0 text-base bg-white p-4 -z-1 duration-300 origin-0 text-gray-400">Contraseña</label>
+                                <label  className="z-1 absolute top-0 text-base bg-white p-4 duration-300 origin-0 text-gray-400">Contraseña</label>
                             </div>
 
                             <button onClick={irDashboard} className="bg-redDerco p-3 text-white font-bold mx-auto rounded-md mt-5 w-full text-lg">Ingresar</button>
