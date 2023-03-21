33ebd3dd4459c53312dd1ee20cf0151b91bf430a
Martin <mar_1905@hotmail.es>
2023-03-21 17:10:15 -0500

    Menu

--- src/pages/Recepcion/components/FormRecCrear.jsx
+++ src/pages/Recepcion/components/FormRecCrear.jsx
@@ -190,7 +190,6 @@ export const InputBasic = ({ pHolder, data, labelName }) => {
                 type="text"
                 placeholder={pHolder}
                 className="w-full border border-gray-300 py-2 px-3 mt-2 rounded-md focus:ring-1 focus:ring-sky-500 outline-none"
-                disabled={true}
             />
         </div>
     )
